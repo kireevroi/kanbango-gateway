50a790731ff143ef199fd46a36579cfd038cf60a
Roman Kireev <kireevroi@gmail.com>
2023-05-01 18:23:19 +0000

    cookies

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,60 @@
 package main
 
+import (
+	"context"
+	// "fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/kireevroi/kanbango/gateway/internal/userproto"
+)
+
+
+
+type user struct {
+	user string `json:"user"`
+	password string `json:"password"`
+}
 
 
 func main() {
 	conn, err := grpc.Dial("kanbango.ru:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Println("WTF happened")
+		log.Print("WTF happened")
 	}
-	// Code removed for brevity
+	defer conn.Close()
+	r := gin.Default()
 
-	// client := pb.NewInventoryClient(conn)
-
-	// Note how we are calling the GetBookList method on the server
-	// This is available to us through the auto-generated code
-	// bookList, err := client.GetBookList(context.Background(), &pb.GetBookListRequest{})
-
-	// log.Printf("book list: %v", bookList)
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	r.POST("/login", func(c *gin.Context) {
+		cookie, err := c.Cookie("auth_cookie")
+		if err != nil {
+			log.Printf("Cookie problems: %v", err)
+			// Send cookie data to login or whatever
+		}
+		log.Printf("Cookie : %v", cookie)
+		var newuser user
+		if err := c.BindJSON(&newuser); err != nil {
+			log.Printf("Something strange happened during login: %v", err)
+		}
+		client := pb.NewUserServiceClient(conn)
+		login, err := client.LoginUser(context.Background(), &pb.LoginUserRequest{Username: newuser.user, Password: newuser.password})
+		if err != nil {
+			log.Printf("Some kind of error getting GRPC: %v", err)
+		}
+		if login.Status == pb.Status_STATUS_OK {
+			c.SetCookie("auth_cookie", login.Uuid, 3600, "/", "kanbango.ru", false, false)
+			c.IndentedJSON(http.StatusOK, gin.H{"Status": "Logged in"})
+		} else {
+			log.Print(login.Status)
+			c.IndentedJSON(http.StatusAccepted, gin.H{"status": login.Status})
+		}
+	})
+	r.Run(":8080")
 }
