e75154947cb0d98d17e4fca1ac0237e76cefd255
coriemis <coriemis@student.21-school.ru>
2023-05-17 21:16:19 +0400

    added 2 endpoints

--- internal/routes/auth.go
+++ internal/routes/auth.go
@@ -60,7 +60,7 @@ func (a *Auth)LoginHandler() gin.HandlerFunc {
 			log.Printf("ingress login error: %v", err)
 		}
 		
-		cookie, _ := c.Cookie("auth_cookie")
+		cookie, _ := c.Cookie("auth")
 		client := pb.NewUserServiceClient(a.conn)
 		log.Println(u.User, cookie)
 		login, err := client.LoginUser(context.Background(), &pb.LoginUserRequest{Username: u.User, Password: u.Password, Uuid: cookie})
@@ -68,12 +68,9 @@ func (a *Auth)LoginHandler() gin.HandlerFunc {
 			log.Printf("login grpc connection error: %v", err)
 			return
 		
-		}
-
-		
-		log.Println(login.Status, login.Uuid)
+		}	
 		if login.Status == pb.Status_STATUS_OK {
-			c.SetCookie("auth_cookie", login.Uuid, 3600, "/", "kanbango.ru", true, true)
+			c.SetCookie("auth", login.Uuid, 3600, "/", "kanbango.ru", true, true)
 			c.JSON(http.StatusOK, gin.H{"Status" : "Logged in"})
 			return
 		} else if login.Status == pb.Status_STATUS_ALRLOGGED {
@@ -90,10 +87,48 @@ func (a *Auth)LoginHandler() gin.HandlerFunc {
 func (a *Auth)LogoutHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		client := pb.NewUserServiceClient(a.conn)
-		cookie, _ := c.Cookie("auth_cookie")
-		log.Println(cookie)
-		client.LogoutUser(context.Background(), &pb.LoginUserRequest{Uuid: cookie})
-		c.SetCookie("auth_cookie", "", -1, "/", "kanbango.ru", false, false)
+		cookie, _ := c.Cookie("auth")
+		client.LogoutUser(context.Background(), &pb.LogoutUserRequest{Uuid: cookie})
+		c.SetCookie("auth", "", -1, "/", "kanbango.ru", true, true)
 		c.JSON(http.StatusOK, gin.H{"Status" : "Logged out"})
+	}
+}
+
+func (a *Auth)SignupHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var u user
+		if err := c.BindJSON(&u); err != nil {
+			log.Printf("ingress singup error: %v", err)
+		}
+		client := pb.NewUserServiceClient(a.conn)
+		_, err := c.Cookie("auth")
+		if err == nil {
+			c.JSON(http.StatusConflict, gin.H{"Status" : "Seems like you are already logged in"})
+			return
+		}
+		cuResponse, err := client.CreateUser(context.Background(), &pb.CreateUserRequest{Username: u.User, Password: u.Password})
+		if cuResponse.Status != pb.Status_STATUS_OK {
+			c.JSON(http.StatusBadRequest, gin.H{"Status" : "Something wrong"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H {"Status" : "User created"})
+	}
+}
+
+func (a *Auth)DeleteHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		client := pb.NewUserServiceClient(a.conn)
+		cookie, err := c.Cookie("auth")
+		if err != nil {
+			c.SetCookie("auth", "", -1, "/", "kanbango.ru", true, true)
+			c.JSON(http.StatusUnauthorized, gin.H {"Status" : "But youre not logged in..."})
+			return
+		}
+		dResponse, err := client.DeleteUser(context.Background(), &pb.LogoutUserRequest{Uuid: cookie})
+		if dResponse.Status != pb.Status_STATUS_OK {
+			c.JSON(http.StatusBadRequest, gin.H{"Status" : "Something wrong"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H {"Status" : "User deleted"})
 	}
 }
